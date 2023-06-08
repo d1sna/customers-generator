e03c8ccefeadc77969ad861435574b889e5521ce
Aleksandr Dis <a.dis@awatera.com>
2023-06-08 13:53:43 +0400

    added encrypt function and constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const CUSTOMERS_COLLECTION_NAME = "customers";
+
+export const CUSTOMERS_ANONYMIZED_COLLECTION_NAME = "customers_anonymized";
+
+export const CUSTOMERS_AUDIT_COLLECTION_NAME = "customers_audit";
---
+++ src/lib/encrypt-string-to-eight-symbols.ts
@@ -0,0 +1,6 @@
+const crypto = require("crypto");
+
+export default function encryptStringToEightSymbols(input: string) {
+  const hash = crypto.createHash("sha256").update(input).digest("hex");
+  return hash.substring(0, 8);
+}
